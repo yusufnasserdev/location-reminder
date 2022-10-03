83a63e2beb7e09441c41c94e8bfb896fdd257754
yusufnasserdev <yusufnassereng@gmail.com>
2022-10-03 05:35:06 +0200

    Updated gradle files, AndroidManifest and AuthenticationActivity

--- LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
+++ LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
@@ -2,7 +2,13 @@ package com.udacity.reminder.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.udacity.reminder.R
+
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.reminder.databinding.ActivityAuthenticationBinding
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,15 +16,40 @@ import com.udacity.reminder.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "AuthenticationActivity"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        binding.lifecycleOwner = this
+
+        binding.signInBtn.setOnClickListener {
+            launchSigningInFlow()
+        }
+    }
+
+    private fun launchSigningInFlow() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), SIGN_IN_RESULT_CODE
+        )
 
     }
 }
