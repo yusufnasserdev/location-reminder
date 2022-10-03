dd17ab4442cbf1f86742133252fcc1297ad19e8f
yusufnasserdev <yusufnassereng@gmail.com>
2022-10-03 05:49:09 +0200

    Finished AuthenticationActivity

--- LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
+++ LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
@@ -1,6 +1,10 @@
 package com.udacity.reminder.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.udacity.reminder.R
@@ -9,6 +13,7 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.reminder.databinding.ActivityAuthenticationBinding
+import com.udacity.reminder.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -51,5 +56,32 @@ class AuthenticationActivity : AppCompatActivity() {
             ).build(), SIGN_IN_RESULT_CODE
         )
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
+                startReminderActivity()
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+                Toast.makeText(this, "Sign in unsuccessful", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startReminderActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
