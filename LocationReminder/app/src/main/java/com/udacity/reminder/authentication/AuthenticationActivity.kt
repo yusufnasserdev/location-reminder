21b0bd045c33ecc34caba7b58e47fd2ba6ef5ddd
yusufnasserdev <yusufnassereng@gmail.com>
2022-10-05 02:19:48 +0200

    Changed user authentication method

--- LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
+++ LocationReminder/app/src/main/java/com/udacity/reminder/authentication/AuthenticationActivity.kt
@@ -1,18 +1,20 @@
 package com.udacity.reminder.authentication
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.reminder.R
 import com.udacity.reminder.databinding.ActivityAuthenticationBinding
 import com.udacity.reminder.locationreminders.RemindersActivity
+
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -22,10 +24,46 @@ class AuthenticationActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "AuthenticationActivity"
-        const val SIGN_IN_RESULT_CODE = 1001
     }
 
     private lateinit var binding: ActivityAuthenticationBinding
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { result: FirebaseAuthUIAuthenticationResult? ->
+        when (result?.resultCode) {
+            RESULT_OK -> {
+                // User successfully signed in
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+                Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
+                startReminderActivity()
+            }
+            else -> {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                Log.i(TAG, "Sign in unsuccessful ${result?.idpResponse?.error}")
+                Toast.makeText(this, "Sign in unsuccessful", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun startSignIn() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+
+        signInLauncher.launch(signInIntent)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,50 +74,7 @@ class AuthenticationActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
         binding.signInBtn.setOnClickListener {
-            launchSigningInFlow()
-        }
-    }
-
-    private fun launchSigningInFlow() {
-        // Give users the option to sign in / register with their email or Google account. If users
-        // choose to register with their email, they will need to create a password as well.
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        // Create and launch sign-in intent. We listen to the response of this activity with the
-        // SIGN_IN_RESULT_CODE code.
-        startActivityForResult(
-            AuthUI
-                .getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(), SIGN_IN_RESULT_CODE
-        )
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == SIGN_IN_RESULT_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                // User successfully signed in
-                Log.i(
-                    TAG,
-                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
-                )
-                Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
-                startReminderActivity()
-
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
-                Toast.makeText(this, "Sign in unsuccessful", Toast.LENGTH_SHORT).show()
-            }
+            startSignIn()
         }
     }
 
